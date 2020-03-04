a660f8397da09c2544af0cdb75fbab6d63c7ad3e
Mic Hussey <mic@confluent.io>
2020-03-04 22:38:38 +0100

    Added status check

--- scripts/invoke_connect_rest.sh
+++ scripts/invoke_connect_rest.sh
@@ -1,5 +1,19 @@
 #/bin/bash
 
+# This script>
+# 1. Reads a JSON template for a Kafka Connect instance configuration
+# 2. Replaces all ${ENV_NAME} with the values from the corresponding environment variable
+# 3. Invokes one of the Kafka Connect REST API methods using the templated data
+#
+# Usage:
+#    invoke_connect_rest.sh (-d | -v | -u)
+#       -d Deletes the connector instance
+#       -s Checks the status of a connector instance
+#       -v Validates the connector configuration
+#       -u Updates an existing connector instance
+#
+# When no parameters are provided a new connector instance is created
+
 if [ -z "$template" ]
 then
 	echo "No template passed to $0, exiting"
@@ -34,8 +48,7 @@ fi
 echo "Using Kafka Connect REST interface at $KAFKA_CONNECT_URL"
 echo "Using Schema Registry interface at $SCHEMA_REGISTRY_URL"
 
-if [ "$1" = "-d" ]
-then
+if [ "$1" = "-d" ]; then
 	echo "Deleting connector $CONNECTOR_NAME"
 	URL=connectors/$CONNECTOR_NAME
 	METHOD=DELETE
@@ -43,12 +56,24 @@ then
 	    -H "Accept:application/json" \
 	    -H  "Content-Type:application/json" \
 	    $KAFKA_CONNECT_URL/$URL
-else
-if [ "$1" = "-v" ]
-	then
-		echo "Validating connector config for $CONNECTOR_NAME"
-		URL=connector-plugins/$CONNECTOR_TYPE/config/validate
-		METHOD=PUT
+	exit
+elif [ "$1" = "-s" ]; then
+	echo "Config of connector $CONNECTOR_NAME"
+	URL=connectors/$CONNECTOR_NAME
+	METHOD=GET
+	RESULT=`curl -s -X $METHOD \
+	    -H "Accept:application/json" \
+	    $KAFKA_CONNECT_URL/$URL `
+	echo $RESULT | jq '.'
+	TASKS_ACTIVE=`echo $RESULT | jq '.tasks | length'`
+	TASKS_GOAL=`echo $RESULT | jq --raw-output '.config."tasks.max"'`
+	echo "There are $TASKS_ACTIVE of $TASKS_GOAL tasks running"
+	
+    exit
+elif [ "$1" = "-v" ]; then
+	echo "Validating connector config for $CONNECTOR_NAME"
+	URL=connector-plugins/$CONNECTOR_TYPE/config/validate
+	METHOD=PUT
 		
 eval "cat <<EOF
 $(< ${template} )
@@ -57,10 +82,24 @@ EOF
 		    -H "Accept:application/json" \
 		    -H  "Content-Type:application/json" \
 		    $KAFKA_CONNECT_URL/$URL --data-binary @-
-	else
-		echo "Creating connector named $CONNECTOR_NAME"
-		URL=connectors/
-		METHOD=POST
+	exit
+elif [ "$1" = "-u" ]; then
+	echo "Updating connector config for $CONNECTOR_NAME"
+	URL=connector-plugins/$CONNECTOR_TYPE/config/validate
+	METHOD=PUT
+		
+eval "cat <<EOF
+$(< ${template} )
+EOF
+" 2> /dev/null | curl -i -X $METHOD \
+		    -H "Accept:application/json" \
+		    -H  "Content-Type:application/json" \
+		    $KAFKA_CONNECT_URL/$URL --data-binary @-
+	exit
+else
+	echo "Creating connector named $CONNECTOR_NAME"
+	URL=connectors/
+	METHOD=POST
 		
 eval "cat <<EOF
 {
@@ -73,7 +112,5 @@ EOF
 		    -H "Accept:application/json" \
 		    -H  "Content-Type:application/json" \
 		    $KAFKA_CONNECT_URL/$URL --data-binary @-
-	fi
+	exit
 fi
-	
-# TODO add Update using PUT!
